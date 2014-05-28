5bc86a0c044675e338303916cf625ad92415cbcf
Sebastian-tbf <Sebastian@tbf.mx>
2014-05-28 15:46:25 -0500

    ajuste de vista productos, inteligencia en grid de catalogo, agregar campos a modelo products

--- app/controllers/catalogo_controller.rb
+++ app/controllers/catalogo_controller.rb
@@ -1,5 +1,6 @@
 class CatalogoController < ApplicationController
 	skip_before_action :authorize
   def index
+  	@products = Product.all
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,11 +1,11 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
 
-
-	def add_product(product_id)
+	#<%= button_to 'Add to Cart', line_items_path(product_id: product, quantity: quantity), remote: true %>
+	def add_product(product_id, quantity)
 		current_item = line_items.find_by(product_id: product_id)
 		if current_item
-			current_item.quantity += 1
+			current_item.quantity = quantity 
 		else
 			current_item = line_items.build(product_id: product_id)
 		end
@@ -21,6 +21,15 @@ class Cart < ActiveRecord::Base
 		#	current_item = line_items.build(product_id: product_id)
 		end
 		current_item
+	end
+	#<%= button_to 'Add to Cart', line_items_path(product_id: product),  method: :delete, remote: true %>
+	def destroy_product(product_id)
+		current_item = line_items.find_by(product_id: product_id)
+		current_item.destroy
+		respond_to do |format|
+      		format.html { redirect_to @line_item.cart, notice: 'Client was successfully destroyed.' }
+      		format.json { head :no_content }
+    	end	
 	end
 	def total_price
 		line_items.to_a.sum { |item| item.total_price }
