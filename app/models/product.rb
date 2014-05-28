005d1ab145ce193459861ac11a3ba4e4f2735546
newesish <hugo@tbf.mx>
2014-05-28 17:00:56 -0500

    se actualizo el CRUD de producto con los nuevos campos.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,17 +3,13 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  #...
-
-  validates :title, :description, :image_url, presence: true
-  validates :unit_price, numericality: {greater_than_or_equal_to: 0.01}
-# 
-  validates :title, uniqueness: true
+  validates :title, :description, presence: true
+  #validates :unit_price, numericality: {greater_than_or_equal_to: 0.01}
   validates :image_url, allow_blank: true, format: {
     with:    %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
-  validates :title, length: {minimum: 10}
+  validates :title, length: {minimum: 3}
 
   def self.latest
     Product.order(:updated_at).last
